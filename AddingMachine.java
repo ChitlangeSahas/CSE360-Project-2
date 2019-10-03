7f0aff0b2d02441740947ef2fd9488d74ae189b5
Sahas Chitlange <chitlangesahas@gmail.com>
2019-10-03 11:29:23 -0700

    commit 1

---
+++ AddingMachine.java
@@ -0,0 +1,59 @@
+package cse360assign2;
+
+/*
+* Adding machine class
+* @author Sahas Chitlange
+* @version 1.0
+* @since 2019-10-03
+*/
+public class AddingMachine {
+
+	private int total;
+
+	/*
+	* AddingMachine constructor constructs
+	* a AddingMachine object when called.
+	*/
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+
+	/*
+	* getTotal function
+	* @returns the total of the "addition" operation
+	*/
+	public int getTotal () {
+		return 0;
+	}
+
+	/*
+	* add funciton
+	* @param value to add
+	*/
+	public void add (int value) {
+		
+	}
+
+	/*
+	* subtract function
+	* @param value to subtract
+	*/
+	public void subtract (int value) {
+		
+	}
+
+	/*
+	* a toString funciton
+	* @return returns string form information
+	*/
+	public String toString () {
+		return "";
+	}
+
+	/*
+	* clears the string / resets the string
+	*/
+	public void clear() {
+	
+	}
+}
